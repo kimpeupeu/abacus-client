88fdd6a910cf621dc97896eb18879d339bc1e317
kimsuyoung <suyoung154@gmail.com>
2023-02-21 11:02:44 +0900

    Added GlobalStyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import "../styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../models/core/store";
 import { ThemeProvider } from "styled-components";
 import { useAppSelector } from "../models/core/hooks";
 import { themes } from "../lib/styles/theme";
+import GlobalStyles from "../lib/styles/GlobalStyles";
+
+import "../styles/globals.css";
 
 const CustomThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const theme = useAppSelector((state) => state.theme.theme);
@@ -17,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <CustomThemeProvider>
         <Component {...pageProps} />
+        <GlobalStyles />
       </CustomThemeProvider>
     </Provider>
   );
