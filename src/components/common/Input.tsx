ce5494a6e60426b4ab4f2f25c6e89c5801cc7ae3
kimsuyoung <suyoung154@gmail.com>
2023-02-21 14:44:23 +0900

    Implement search ui

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,90 @@
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import styled, { css } from "styled-components";
+import media from "../../lib/styles/media";
+
+export interface InputProps {
+  className?: string;
+  large?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({ className, large }) => {
+  return (
+    <Block className={className} large>
+      <FontAwesomeIcon icon={solid("search")} />
+      <input />
+    </Block>
+  );
+};
+
+const Block = styled.div<{ large?: boolean }>`
+  display: flex;
+  height: 2.25rem;
+  border: 1px solid ${({ theme }) => theme.border2};
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+  align-items: center;
+  transition: all 0.125s ease-in;
+  cursor: text;
+  svg {
+    transition: all 0.125s ease-in;
+    margin-right: 0.5rem;
+    width: 1rem;
+    height: 1rem;
+    fill: ${({ theme }) => theme.text1};
+    flex-shrink: 0;
+  }
+  input {
+    transition: all 0.125s ease-in;
+    font-size: 0.875rem;
+    flex: 1;
+    display: block;
+    line-height: 1rem;
+    height: 1rem;
+    padding: 0;
+    border: none;
+    outline: 0;
+    background: transparent;
+    color: ${({ theme }) => theme.text2};
+    min-width: 0;
+    &::placeholder {
+      color: ${({ theme }) => theme.text3};
+    }
+  }
+
+  ${(props) =>
+    props.large &&
+    css`
+      height: 4rem;
+      padding: 0 1.5rem;
+      svg {
+        width: 1.5rem;
+        height: 1.5rem;
+        margin-right: 1.25rem;
+      }
+      input {
+        font-size: 1.5rem;
+        line-height: 2rem;
+        height: 2rem;
+      }
+      ${media.small} {
+        height: 2.25rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        svg {
+          width: 1rem;
+          height: 1rem;
+          margin-right: 0.75rem;
+        }
+        input {
+          flex: 1;
+          font-size: 1.125rem;
+          line-height: 1.5;
+          height: auto;
+        }
+      }
+    `}
+`;
+
+export default Input;
--- src/components/home/HomeSearch.tsx
+++ src/components/home/HomeSearch.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import { mediaQuery } from "../../lib/styles/media";
+import Input from "../common/Input";
 
 export interface HomeSearchProps {}
 
 const HomeSearch: React.FC<HomeSearchProps> = ({}) => {
-  return <></>;
+  return (
+    <Block>
+      <SearchInput large />
+    </Block>
+  );
 };
+
+const Block = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
+
+const SearchInput = styled(Input)`
+  width: 800px;
+
+  ${mediaQuery(1919)} {
+    width: 600px;
+  }
+  ${mediaQuery(1440)} {
+    width: 400px;
+  }
+  ${mediaQuery(1056)} {
+    width: calc(100% - 2rem);
+  }
+`;
 
 export default HomeSearch;
